ea4668396edf5912864eca8e81b0f67f741c06e2
maby0801 <maby0801@student.miun.se>
2018-09-12 14:36:24 +0200

    Gulp concat, Gulp uglify, Gulp uglify css installed. Package-lock.json updated. Gulpfile.js created.

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// DT173G Webbutveckling III
+// Moment 2 - NodeJs & Gulp
+// Mattias Bygdeson
+
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const uglifycss = require('gulp-uglifycss');
+
+gulp.task('default', ['update'], function(){
+    // Activate all automation
+});
+
+// Move all html-files to pub
+gulp.task('copy_html', function(){
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('pub/'))
+});
+
+// Concatenate and compress all javascript-files and move to pub
+gulp.task('convert_js', function(){
+    return gulp.src('src/js/*.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('pub/js'))
+});
+
+// Concatenate and compress all css-files and move to pub
+gulp.task('convert_css', function(){
+    return gulp.src('src/css/*.css')
+        .pipe(concat('main.min.css'))
+        .pipe(uglifycss())
+        .pipe(gulp.dest('pub/css'))
+});
+
+// Look for changes in the file system
+gulp.task('update', function(){
+    gulp.watch('src/js/*.js', ['convert_js']);
+    gulp.watch('src/css/*.css', ['convert_css']);
+    gulp.watch('src/*.html', ['copy_html']);
+})
